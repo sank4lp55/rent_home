1a6e8af1232f2c939bb09d46cb1f8160c745121e
sank4lp55 <sankalpsrivastav55@gmail.com>
2024-10-08 01:02:42 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/rent_home/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rent_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupPage extends StatefulWidget {
+  const LoginSignupPage({super.key});
+
+  @override
+  _LoginSignupPageState createState() => _LoginSignupPageState();
+}
+
+class _LoginSignupPageState extends State<LoginSignupPage> {
+  bool isLogin = true; // Toggle between login and signup
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Background Image Stack
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/onboard.png", // Replace with your image
+                fit: BoxFit.cover,
+                height: 450,
+              ),
+            ),
+            // White Opacity Layer
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 450, // Match the height of the image
+                color: Colors.white.withOpacity(0.6), // White opacity
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 15,
+                        backgroundColor: theme.primaryColor,
+                        child: const Icon(
+                          Icons.home,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        "RentHome",
+                        style: theme.textTheme.titleLarge?.copyWith(
+                          color: theme.primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // Bottom Rounded Container for Form
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.all(24.0),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0), // Circular top edges
+                    topRight: Radius.circular(40.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      offset: Offset(0, -5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Form Section
+                    Text(
+                      isLogin ? "Login to Your Account" : "Create Your Account",
+                      style: theme.textTheme.headlineMedium?.copyWith(
+                        color: theme.primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      isLogin
+                          ? "Welcome back! Please enter your credentials to log in."
+                          : "Sign up to explore the best homes.",
+                      style: theme.textTheme.bodyLarge?.copyWith(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          // Email Field
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              prefixIcon:
+                                  Icon(Icons.email, color: theme.primaryColor),
+                              hintText: "Email",
+                              filled: true,
+                              fillColor: Colors.grey[100],
+                              contentPadding:
+                                  const EdgeInsets.symmetric(vertical: 18),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+
+                          // Password Field
+                          TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              prefixIcon:
+                                  Icon(Icons.lock, color: theme.primaryColor),
+                              hintText: "Password",
+                              filled: true,
+                              fillColor: Colors.grey[100],
+                              contentPadding:
+                                  const EdgeInsets.symmetric(vertical: 18),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                          ),
+
+                          // Confirm Password Field (Signup only)
+                          if (!isLogin)
+                            Padding(
+                              padding: const EdgeInsets.only(top: 16),
+                              child: TextFormField(
+                                controller: confirmPasswordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.lock,
+                                      color: theme.primaryColor),
+                                  hintText: "Confirm Password",
+                                  filled: true,
+                                  fillColor: Colors.grey[100],
+                                  contentPadding:
+                                      const EdgeInsets.symmetric(vertical: 18),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value != passwordController.text) {
+                                    return 'Passwords do not match';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Sign Up or Login Button (styled like Get Started)
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Handle login or signup logic
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: theme.primaryColor,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: const StadiumBorder(),
+                        ),
+                        child: Text(
+                          isLogin ? "Login" : "Sign Up",
+                          style: theme.textTheme.titleMedium?.copyWith(
+                            color: theme.colorScheme.onPrimary,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Toggle between Login/Signup
+                    const SizedBox(height: 20),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            isLogin = !isLogin;
+                          });
+                        },
+                        child: Text(
+                          isLogin
+                              ? "Don't have an account? Sign up"
+                              : "Already have an account? Login",
+                          style: theme.textTheme.bodyLarge?.copyWith(
+                            color: theme.primaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
